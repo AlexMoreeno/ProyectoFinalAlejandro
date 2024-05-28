cdbb97b9066d5cf4e3529258b33b2f72160ffb9a
AlexMoreeno <alejandromorenoparedes@gmail.com>
2024-05-28 12:36:07 +0200

    Modificacionesa en la parte visual -> Comprobaciones finales 5

--- src/proyectofinalalejandro/ProyectoFinalAlejandro.java
+++ src/proyectofinalalejandro/ProyectoFinalAlejandro.java
@@ -15,6 +15,7 @@ public class ProyectoFinalAlejandro {
     public static void main(String[] args) {
          try {
           //  UIManager.setLookAndFeel( new FlatLightLaf() );
+          //  tema añadido
             FlatArcDarkIJTheme.setup();
         } catch( Exception ex ) {
             System.err.println( "Failed to initialize LaF" );
